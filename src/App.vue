e7eea75020be1455d03dff8b353f3a23a31436e8
Thel Nu Aein <thelnuaein5450.com>
2022-03-31 20:45:09 +0630

    record

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,41 @@
           <div class="card-body">
             <h3 class="card-title mb-3">ကုန်စည်း</h3>
             <div class="mb-3">
-              <form action="">
-                <select name="" id="" class="form-select" v-model="selectedProduct">
+              <form action="" @submit.prevent="saveRecord">
+                <select  class="form-select mb-3" v-model="selectedProduct">
                   <option value="">Select Product</option>
                   <option v-for="product in products" :value="product.id" :key="product.id">{{product.name}}</option>
                 </select>
-                <div class="">
-                  <select v-model="selectedUnit" class="form-select">
-                    <option v-if="selectedProduct === 0 ">Select Unit</option>
-                    <option v-for="unit in selectedProductDetail.unit" :key="unit.id" >
-                      {{unit.name}}
+                <div class="mb-3" v-if="selectedProduct > 0">
+<!--                  <div class="form-check" v-for="unit in selectedProductDetail.unit">-->
+<!--                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">-->
+<!--                    <label class="form-check-label" for="flexRadioDefault1">-->
+<!--                      {{unit.name}}[{{unit.price}}kyat]-->
+<!--                    </label>-->
+<!--                  </div>-->
+                  <select v-model="selectedUnit" class="form-select mb-3">
+                    <option value="">Select Unit</option>
+                    <option v-for="unit in selectedProduct>0 && selectedProductDetail.unit" :value="unit.id" :key="unit.id" >
+                      {{unit.name}} - [{{unit.price}}ကျပ်]
                     </option>
                   </select>
                 </div>
 
+<!--                <div v-else>-->
+<!--                  <select class="form-select mb-3">-->
+<!--                    <option value="">Select Unit</option>-->
+<!--                  </select>-->
+<!--                </div>-->
+
+                <div class="mb-3">
+                  <input  class="form-control" type="number" v-model="inputQuantity" placeholder="quantity">
+                </div>
+                <div class="" @click="saveRecord">
+                  <button class="btn btn-primary">Add</button>
+                </div>
               </form>
+              {{records}}
+
             </div>
 
           </div>
@@ -40,7 +60,8 @@ export default {
     return {
       selectedProduct:'',
       selectedUnit:'',
-
+      inputQuantity:null,
+      records:[],
       products: [
         {
           id: 1,
@@ -118,12 +139,29 @@ export default {
   },
   computed: {
     selectedProductDetail() {
-      if (this.selectedProduct === ''){
+      if (this.selectedProduct === null){
         return {}
       }
       return this.products.find(el=>el.id === this.selectedProduct);
     }
   },
+  methods: {
+    saveRecord() {
+      let currentUnit = this.selectedProductDetail.unit.find(el=>el.id === this.selectedUnit)
+      let record = {
+        product:this.selectedProductDetail,
+        unit:currentUnit,
+        quantity:this.inputQuantity,
+        cost:currentUnit.price *this.inputQuantity
+      }
+      this.records=[...this.records,record]
+
+      console.log(record);
+
+      this.selectedProduct=this.selectedUnit=this.inputQuantity=''
+    }
+  },
+
 }
 </script>
 
