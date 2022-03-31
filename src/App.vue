17d581550b6db5f4619053ee09f6242c4707d521
Thel Nu Aein <thelnuaein5450.com>
2022-04-01 00:06:42 +0630

    tab,del,total

--- src/App.vue
+++ src/App.vue
@@ -2,50 +2,83 @@
   <div class="container">
     <div class="row my-3">
       <div class="col-lg-8">
-
+        <table class="table table-bordered table-striped table-primary">
+          <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Unit</th>
+            <th>Unit-Price</th>
+            <th>Quantity</th>
+            <th>Cost</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="record in records" class="animate__animated animate__fadeInDown" :key="record.id">
+            <td>
+              <span>{{record.id}}</span>
+              <i @click="del(record.id)" class="fa-solid fa-trash text-danger"></i>
+            </td>
+            <td>{{record.product.name}}</td>
+            <td>{{record.unit.name}}</td>
+            <td>{{record.unit.price}}ကျပ်</td>
+            <td>{{record.quantity}}</td>
+            <td>{{record.cost}}</td>
+          </tr>
+          </tbody>
+          <tfoot>
+          <tr v-if="records.length > 0">
+            <td colspan="5">Total</td>
+            <td>{{records.reduce((up,c)=>up+c.cost,0)}}</td>
+          </tr>
+          </tfoot>
+        </table>
       </div>
       <div class="col-lg-4">
         <div class="card">
           <div class="card-body">
-            <h3 class="card-title mb-3">ကုန်စည်း</h3>
-            <div class="mb-3">
-              <form action="" @submit.prevent="saveRecord">
-                <select  class="form-select mb-3" v-model="selectedProduct">
-                  <option value="">Select Product</option>
-                  <option v-for="product in products" :value="product.id" :key="product.id">{{product.name}}</option>
+            <h3 class="card-title mb-3">ကုန်ပစ်စည်း</h3>
+            <form action="" @submit.prevent="saveRecord">
+              <div class="my-3">
+                <select class="form-select mb-3" v-model="selectedProduct" id="">
+                  <option value="">Selected Product</option>
+                  <option v-for="product in products" :value="product.id" :key="product.id">
+                    {{product.name}}
+                  </option>
                 </select>
-                <div class="mb-3" v-if="selectedProduct > 0">
-<!--                  <div class="form-check" v-for="unit in selectedProductDetail.unit">-->
-<!--                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">-->
-<!--                    <label class="form-check-label" for="flexRadioDefault1">-->
-<!--                      {{unit.name}}[{{unit.price}}kyat]-->
-<!--                    </label>-->
-<!--                  </div>-->
-                  <select v-model="selectedUnit" class="form-select mb-3">
+                <!--                <div>-->
+                <!--                  <select class="form-select" v-model="selectedUnit">-->
+                <!--                    <option v-if="selectedProduct === 0">Select Unit</option>-->
+                <!--                    <option v-else="selectedProduct >0" v-for="unit in selectedProductDetail.unit" :key="unit.id" >-->
+                <!--                      {{unit.name}}-->
+                <!--                    </option>-->
+
+                <!--                  </select>-->
+                <!--                </div>-->
+                <div v-if="selectedProduct >0">
+                  <select class="form-select mb-3" v-model="selectedUnit" id="">
                     <option value="">Select Unit</option>
-                    <option v-for="unit in selectedProduct>0 && selectedProductDetail.unit" :value="unit.id" :key="unit.id" >
+                    <option v-for="unit in selectedProduct>0 && selectedProductDetail.unit" :value="unit.id">
                       {{unit.name}} - [{{unit.price}}ကျပ်]
                     </option>
                   </select>
                 </div>
-
-<!--                <div v-else>-->
-<!--                  <select class="form-select mb-3">-->
-<!--                    <option value="">Select Unit</option>-->
-<!--                  </select>-->
-<!--                </div>-->
+                <!--                <div v-else>-->
+                <!--                  <select class="form-select mb-3" id="">-->
+                <!--                    <option value="">Select Unit</option>-->
+                <!--                  </select>-->
+                <!--                </div>-->
 
                 <div class="mb-3">
-                  <input  class="form-control" type="number" v-model="inputQuantity" placeholder="quantity">
+                  <input type="number" placeholder="quantity" class="form-control" v-model="inputQuantity">
                 </div>
-                <div class="" @click="saveRecord">
-                  <button class="btn btn-primary">Add</button>
-                </div>
-              </form>
-              {{records}}
+              </div>
+              <div class="" @click="saveRecord">
+                <button class="btn btn-primary">Add</button>
+              </div>
 
-            </div>
-
+            </form>
+<!--            {{records}}-->
           </div>
         </div>
       </div>
@@ -58,83 +91,93 @@
 export default {
   data() {
     return {
-      selectedProduct:'',
-      selectedUnit:'',
+      selectedProduct:"",
+      selectedUnit:"",
       inputQuantity:null,
-      records:[],
+      recordStart:1,
       products: [
         {
-          id: 1,
-          name:'Apple',
+          id:1,
+          name:"apple",
           unit:[
             {
-              name:'လုံး',
+              id:1,
+              name:"လုံး",
               price:110
             },
             {
-              name:'ထုပ်',
+              id:2,
+              name:"ထုပ်",
               price:1000
             },
           ]
         },
         {
-          id: 2,
-          name:'Egg',
+          id:2,
+          name:"egg",
           unit:[
             {
-              name:'လုံး',
-              price:160
+              id:1,
+              name:"လုံး",
+              price:150
             },
             {
-              name:'ထုပ်',
-              price:1500
+              id:2,
+              name:"ကဒ်",
+              price:1200
             },
           ]
         },
         {
-          id: 3,
-          name:'Milk',
+          id:3,
+          name:"ice-cream",
           unit:[
             {
-              name:'ဘူး',
-              price:1000
-            },
-            {
-              name:'ကပ်',
-              price:5000
-            },
-          ]
-        },
-        {
-          id: 4,
-          name:'cheeseball',
-          unit:[
-            {
-              name:'လုံး',
-              price:700
-            },
-            {
-              name:'ထုပ်',
-              price:10000
-            },
-          ]
-        },
-        {
-          id: 5,
-          name:'ice-cream',
-          unit:[
-            {
-              name:'ဘူးေသး',
+              id:1,
+              name:"ခွက်-ေသး",
               price:500
             },
             {
-              name:'ဘူးကြီး',
-              price:2000
+              id:2,
+              name:"ခွက်-ကြီး",
+              price:1000
             },
           ]
-        }
-
-      ]
+        },
+        {
+          id:4,
+          name:"cheese-ball",
+          unit:[
+            {
+              id:1,
+              name:"ထုပ်-ေသး",
+              price:700
+            },
+            {
+              id:2,
+              name:"ထုပ်-ကြီး",
+              price:14000
+            },
+          ]
+        },
+        {
+          id:5,
+          name:"ခေါက်ဆွဲ",
+          unit:[
+            {
+              id:1,
+              name:"ထုပ်",
+              price:500
+            },
+            {
+              id:2,
+              name:"ဒါဇင်",
+              price:4000
+            }
+          ]
+        },
+      ],
+      records:[],
     }
   },
   computed: {
@@ -142,30 +185,41 @@ export default {
       if (this.selectedProduct === null){
         return {}
       }
-      return this.products.find(el=>el.id === this.selectedProduct);
+      return this.products.find(el=>el.id === this.selectedProduct)
     }
   },
   methods: {
+
     saveRecord() {
-      let currentUnit = this.selectedProductDetail.unit.find(el=>el.id === this.selectedUnit)
-      let record = {
+      let currentUnit=this.selectedProductDetail.unit.find(el=>el.id === this.selectedUnit)
+      let record={
+        id:this.recordStart,
         product:this.selectedProductDetail,
         unit:currentUnit,
         quantity:this.inputQuantity,
-        cost:currentUnit.price *this.inputQuantity
-      }
+        cost:currentUnit.price * this.inputQuantity
+      };
+      //this.records.push(record);
       this.records=[...this.records,record]
-
       console.log(record);
 
-      this.selectedProduct=this.selectedUnit=this.inputQuantity=''
+      this.selectedProduct=this.selectedUnit=this.inputQuantity=""
+      this.recordStart++
+
+      // console.log(this.selectedProductDetail,this.selectedProduct,this.selectedUnit,this.inputQuantity)
+    },
+    del(recordId){
+      this.records = this.records.filter(record => record.id != recordId)
+      console.log(recordId)
     }
   },
-
 }
 </script>
 
 <style lang="scss">
-@import "~bootstrap/dist/css/bootstrap.min.css";
-@import "~@fortawesome/fontawesome-free/css/all.min.css";
+
+$primary : #6a4c93;
+@import "~bootstrap/scss/bootstrap";
+@import "~@fortawesome/fontawesome-free";
+@import "~animate.css/animate.min.css";
 </style>
